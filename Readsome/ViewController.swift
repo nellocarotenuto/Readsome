6c40582eed129515775a1d61ae0ea92c9457e872
rosarusso <rosa.russo995@gmail.com>
2018-03-05 16:57:13 +0100

    Storyboard: Text size slider added

--- Readsome/ViewController.swift
+++ Readsome/ViewController.swift
@@ -11,11 +11,24 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var sliderValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+//        let selectedValue = String(sender.value)
+        
+        let selectedValue = sender.value*10
+        
+        sliderValue.font = sliderValue.font.withSize(CGFloat(selectedValue))
+//        sliderValue.text = String(stringInterpolationSegment: selectedValue)
+//        label.font = label.font.fontWithSize(20)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
